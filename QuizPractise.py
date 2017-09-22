00050edb316eeeec5f9639e3aaeaf0b52ab0948a
Vaijyant Tomar <vaijyant.tomar@gmail.com>
2017-09-22 05:07:31 -0400

    Initial Commit

--- QuizPractise.py
+++ QuizPractise.py
@@ -1,48 +1,56 @@
+# -*- coding: utf-8 -*-
 """
 Created on Thu Sep 21 20:20:49 2017
 
-@author: vaijy
+@author: Vaijyant Tomar
 """
-import os
-import csv
+
 import numpy as np
+import pandas as pd
 
-# 1. Reading a file
-path = "C:\\users\\vaijy\\Desktop"
-file_name = "dataset_1.csv"
-
-os.chdir(path)
-
-x = np.array([], dtype=float)
-y = np.array([], dtype=float)
-z = np.array([], dtype=float)
-
-with open(file_name) as csvDataFile:
-    csvReader = csv.reader(csvDataFile)
-    header = next(csvReader)
-    for row in csvReader:
-        x = np.append(x, float(row[0]))
-        y = np.append(y, float(row[1]))
-        z = np.append(z, float(row[2]))
+#1. loading data
+data = pd.read_csv(r"C:\users\vaijy\Desktop\dataset_1.csv")
+tdata = data.values
+tdata = tdata.T
 
 
-# 2. Calulating sample_variance
+#2. Calulating sample_variance
 def sample_variance(x):
     n = len(x)
     x_mean = sum(x) / n
-    var = sum((x - x_mean) ** 2) / (n - 1)
+    var = sum((x-x_mean)**2) / (n - 1)
     return var
 
+#3. mean vector
+def mean_vector(mean_x, mean_y, mean_z):
+    mean_vector = np.array([[mean_x],[mean_y],[mean_z]])
+    return mean_vector
 
-# 3. Covariance matrix
-def covariance_matrix(x, y):
-    cov_matrix = np.cov(x, y)
-    return cov_matrix
+#4. mean centering
+def mean_center(matrix, mean_vector):
+    return matrix - mean_vector
 
+#5. calculating covariance_matrix
+def covariance_matrix(matrix):
+    cov_mat = np.cov([matrix[0,:], matrix[1,:], matrix[2,:]])
+    return cov_mat
+
+#6. calculating eigen values and eigen vectors
+def eigen_value_vector(matrix):
+    cov_mat = covariance_matrix(matrix)
+    eig_val_cov, eig_vec_cov = np.linalg.eig(cov_mat)
+    for i in range(len(eig_vec_cov)):
+        eigvec_cov = eig_vec_cov[:,i].reshape(1,3).T
+        print('Eigenvector {}: \n{}'.format(i+1, eigvec_cov))
+        print('Eigenvalue {} matrix: {}\n'.format(i+1, eig_val_cov[i]))
 
 # -----------------------------------------------------------------------------
-print(sample_variance(x))
-print(sample_variance(y))
-print(sample_variance(z))
-print("---------------------------------")
-print(covariance_matrix(x, y))
+print(sample_variance(tdata[0,:]))
+print(sample_variance(tdata[1,:]))
+print(sample_variance(tdata[2,:]))
+
+mean_vector = mean_vector(np.mean(tdata[:,0]), np.mean(tdata[:,1]), np.mean(tdata[:,2]))
+print(covariance_matrix(tdata))
+eigen_value_vector(tdata)
+
+mean_center(tdata, mean_vector)
